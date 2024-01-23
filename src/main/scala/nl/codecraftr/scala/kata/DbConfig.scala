fa928e7e001fffe12fdbca190447a0a07949b139
Roy Straub <rstraub@xebia.com>
2024-01-23 13:55:30 +0100

    Formatting, continued

--- src/main/scala/nl/codecraftr/scala/kata/DbConfig.scala
+++ src/main/scala/nl/codecraftr/scala/kata/DbConfig.scala
@@ -1,8 +1,5 @@
 package nl.codecraftr.scala.kata
 
-import pureconfig._
-import pureconfig.generic.auto._
-
 case class DbConfig(
     host: String,
     port: Int,
--- src/main/scala/nl/codecraftr/scala/kata/Todo.scala
+++ src/main/scala/nl/codecraftr/scala/kata/Todo.scala
@@ -11,7 +11,13 @@ case class Todo(
 object Todos {
   lazy val todos: List[Todo] = List(
     Todo(Timestamp.valueOf(LocalDateTime.now()), "Implement the kata"),
-    Todo(Timestamp.valueOf(LocalDateTime.now().plusDays(1)), "Write a blog post about the kata"),
-    Todo(Timestamp.valueOf(LocalDateTime.now().plusDays(2)), "Publish the blog post")
+    Todo(
+      Timestamp.valueOf(LocalDateTime.now().plusDays(1)),
+      "Write a blog post about the kata"
+    ),
+    Todo(
+      Timestamp.valueOf(LocalDateTime.now().plusDays(2)),
+      "Publish the blog post"
+    )
   )
 }
